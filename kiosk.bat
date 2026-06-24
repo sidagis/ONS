3d6bec1eccd03dbdde614889dadb117da4bb9f55
sidagis <sigbjorndale@gmail.com>
2026-06-24 12:05:45 +0200

    Update kiosk.bat

--- kiosk.bat
+++ kiosk.bat
@@ -1,6 +1,11 @@
 @echo off
+set "URL=https://raw.githubusercontent.com/sidagis/ONS/refs/heads/main/theMap.html"
+set "DEST=C:\theMap.html"
+
+curl -L -o "%DEST%" "%URL%"
+
 start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" ^
-  --kiosk "file:///C:/theMap.html" ^
+  --kiosk "file:///C:/kiosk/theMap.html" ^
   --edge-kiosk-type=fullscreen ^
   --no-first-run ^
   --kiosk-idle-timeout-minutes=0 ^
